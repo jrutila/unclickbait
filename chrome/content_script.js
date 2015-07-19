b2d918c338c2f8c49ba818b78a35a25bd3f185be
Juho Rutila <juho.rutila@iki.fi>
2015-07-19 16:54:54 +0300

    Gets the UnClickbait titles when a page is loaded
    And replaces them in the page.

--- chrome/content_script.js
+++ chrome/content_script.js
@@ -1,29 +1,52 @@
 if (window == top) {
  chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
-  sendResponse(findLinks_all());
+   var link_selector = {
+    "www.ampparit.com": "a.news-item-headline, a.__sidebar-item"
+   };
+
+   var sele = link_selector[window.location.hostname];
+
+   if (typeof sele === "undefined")
+    sele = "a";
+
+  var urls = [];
+  $(sele).each(function() {
+    var href = $(this).attr("href");
+    if (! (/^http/).test(href))
+      href = window.location.origin + href;
+    urls.push(href);
+  });
+  
+  console.log(urls);
+  var data = { url: urls };
+  var xhr = new XMLHttpRequest();
+  // TODO: Get the url from options or something
+  xhr.open("POST", "https://unclickbait-jrutila.c9.io/clickbait/search", true);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.onreadystatechange = function(msg, res) {
+    if (xhr.readyState == 4) {
+      var res = JSON.parse(xhr.responseText);
+      for (var r in res) {
+        var cb = res[r];
+        var $elem = $("[href='"+cb.url.replace(window.location.origin, "")+"'], [href='"+cb.url+"'] ");
+        var orig = $elem.html();
+        // TODO: Replace only the origin text and not for example the time info
+        $elem.html(cb.titles[0].text);
+        $elem.append("<img src='"+chrome.extension.getURL("images/clickbait.png")+"'/>");
+      }
+      console.log(res);
+    }
+    // TODO: Error case
+  };
+  xhr.send(JSON.stringify(data));
+
+  sendResponse(findLinks(sele));
  });
 }
 
-var findLinks_all = function() {
+var findLinks = function(sele) {
  var links = [];
- $("a").each(function(){
-  links.push({
-   ref: document.location.href,
-   text: $(this).text(),
-   url: $(this).prop("href")
-  });
- });
- return links;
-};
-
-var findLinks = function() {
- var link_selector = {
-  "www.ampparit.com": "a.news-item-headline"
- };
- if (typeof link_selector[window.location.hostname] === "undefined") return;
-
- var links = [];
- $(link_selector[window.location.hostname]).each(function(){
+ $(sele).each(function(){
   links.push({
    ref: document.location.href,
    text: $(this).text(),
--- chrome/popup.js
+++ chrome/popup.js
@@ -4,7 +4,27 @@ $(function(){
 });
 $(function(){
  $("button").click(function(){
-  console.log(chrome.extension.getBackgroundPage().selectedLink, $("#text").val());
-  alert("inspect popup, and check console");
+  // data is something like this:
+  // {
+  //  url: the url of the page
+  //  href: (optional) where did the user get here
+  //  originalText: (optional) what was in the original link
+  //  text: the proposed new title
+  // }
+  var data = chrome.extension.getBackgroundPage().selectedLink;
+  data.originalText = data.text;
+  data.text = $("#text").val();
+  console.log(data);
+  var xhr = new XMLHttpRequest();
+  // TODO: Get the url from options or something
+  xhr.open("POST", "https://unclickbait-jrutila.c9.io/clickbait/add", true);
+  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhr.onreadystatechange = function(msg) {
+    if (xhr.readyState == 4) {
+      alert("New UnClickbait title added! Thank you!");
+    }
+    // TODO: Error case
+  };
+  xhr.send(JSON.stringify(data));
  });
 });
